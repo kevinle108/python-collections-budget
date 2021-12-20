52e9a86e7c9bd438e36cbbe31eea6cecbbb8b299
Kevin Le <kevinle108@gmail.com>
2021-12-19 21:33:39 -0500

    fixed top5

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -8,7 +8,8 @@ spending_categories = []
 for expense in expenses.list:
   spending_categories.append(expense.category)
 spending_counter = collections.Counter(spending_categories)
-categories, count = zip(*spending_counter.most_common(5))
+top5 = spending_counter.most_common(5)
+categories, count = zip(*top5)
 fig, ax = plt.subplots()
 ax.bar(categories, count)
 ax.set_title('# of Purchases by Category')
